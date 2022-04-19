8ad193c3cd3d61e000f33a3599f6361c2d062502
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-04-19 23:04:39 +0300

    Added Customer and Engine Models

--- yourMechanic/models.py
+++ yourMechanic/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from cloudinary.models import CloudinaryField
 
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+phone_number_validator = RegexValidator(
+	regex=r'^[0-9 \(\)]{10,12}$', message="Phone numbers must begin with +2547.... or 07..."
+)
 # Create your models here.
+
+class Customer(models.Model):
+    username = models.OneToOneField(User, on_delete=models.CASCADE, blank=True,related_name='customer_name')
+    description = models.TextField(blank=True)
+    contact = models.CharField(max_length=15, blank=True, validators=[phone_number_validator])
+    location = models.CharField(max_length=100, null=True, blank=True)
+    image = CloudinaryField('image', blank=True)
+    
+    def __str__(self):
+        return str(self.username.username)
+
+class Engine(models.Model):
+    part = models.CharField(max_length=100)
+    description = models.TextField(null=True,blank=True)
+    image = CloudinaryField('image', blank=True)
