1c0d0ca032d59d2ee7b38167b708a4d65feb8aac
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-04-19 22:12:05 +0300

    Created Postgress DB and updated mechanic settings with the database name,user and password

--- mechanic/settings.py
+++ mechanic/settings.py
@@ -75,10 +75,13 @@ WSGI_APPLICATION = 'mechanic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mechanic',
+        'USER': 'user',
+        'PASSWORD':'Mzazi25',
     }
 }
+
 
 
 # Password validation
