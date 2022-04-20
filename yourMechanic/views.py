5342dacbe26bedd218a768f3bfe9711865bc1dba
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-04-20 10:30:55 +0300

    Added views for the fullbodypaint

--- yourMechanic/views.py
+++ yourMechanic/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import  FullBodyPaint
+from .serializer import FullBodyPaintSerializer
 
 # Create your views here.
+
+class FullBodyPaintList(APIView):
+    def get(self, request, format=None):
+        paint = FullBodyPaint.objects.all()
+        serializers = FullBodyPaintSerializer(paint, many=True)
+        return Response(serializers.data)
