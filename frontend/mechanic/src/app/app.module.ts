f85d528802ada57b0931b3a81df3b95fd73779a7
Teri11 <augustineteri157@gmail.com>
2022-04-20 08:08:07 +0300

    added cards and background image

--- frontend/mechanic/src/app/app.module.ts
+++ frontend/mechanic/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationBarComponent
+    NavigationBarComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
