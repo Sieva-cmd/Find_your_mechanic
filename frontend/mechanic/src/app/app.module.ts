d5bcb136014d2f3cd3ff754cb8d3a58ef815a7b6
Teri11 <augustineteri157@gmail.com>
2022-04-20 08:50:50 +0300

    initial commit

--- frontend/mechanic/src/app/app.module.ts
+++ frontend/mechanic/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { LandingComponent } from './landing/landing.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
-    LandingComponent
+    LandingComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
