1d1eb8c6989deb9359d0b8117c6c957366da6e5e
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-04-20 10:10:06 +0300

    Added Django Rest Framework to mechanic app

--- yourMechanic/models.py
+++ yourMechanic/models.py
@@ -1,25 +1,19 @@
 from django.db import models
 from django.core.validators import RegexValidator
 from cloudinary.models import CloudinaryField
-
 from django.contrib.auth import get_user_model
-
 User = get_user_model()
-
-
 phone_number_validator = RegexValidator(
-	regex=r'^[0-9 \(\)]{10,12}$', message="Phone numbers must begin with +2547.... or 07..."
+    regex=r'^[0-9 \(\)]{10,12}$', message="Phone numbers must begin with +2547.... or 07..."
 )
 # Create your models here.
-
 class Customer(models.Model):
-    username = models.OneToOneField(User, on_delete=models.CASCADE, blank=True,related_name='customer_name')
+    username = models.OneToOneField(User, on_delete=models.CASCADE, blank=True,related_name='name')
     description = models.TextField(blank=True)
     phone_number = models.CharField(max_length=15, blank=True, validators=[phone_number_validator])
     email = models.EmailField()
     location = models.CharField(max_length=100, null=True, blank=True)
     image = CloudinaryField('image', blank=True)
-    
     def __str__(self):
         return str(self.username.username)
 class Mechanic(models.Model):
@@ -27,14 +21,11 @@ class Mechanic(models.Model):
     speciality = models.TextField(blank=True)
     phone_number = models.CharField(max_length=15, blank=True, validators=[phone_number_validator])
     location = models.CharField(max_length=100, null=True, blank=True)
-    
     def __str__(self):
         return str(self.name.name)
-
 class Engine(models.Model):
     part = models.CharField(max_length=100)
     description = models.TextField(null=True,blank=True)
-    image = CloudinaryField('image', blank=True)
-
+    image = CloudinaryField('photo', blank=True)
     def __str__(self):
         return str(self.part)
